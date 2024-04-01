6d8ae5eb8f134f4333478525666b9dcefa0dc3e0
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-04-01 10:42:36 -0300

    removing card interface

--- src/model/card.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseCard } from "./baseCard.model";
-import { CardPosition } from "./cardPosition.model";
-import { Challenger } from "./challenger.model.";
-
-export interface Card extends BaseCard {
-    constructor(): Card;
-}
